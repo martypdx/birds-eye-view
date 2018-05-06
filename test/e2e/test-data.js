2d27980c1e8505f2075159cf0acb1e80b987cc68
Keli Hansen <keli.p.hansen@gmail.com>
2018-05-06 08:03:13 -0700

    add common data-posting function and data

---
+++ test/e2e/test-data.js
@@ -0,0 +1,48 @@
+module.exports = {
+    item1Data: {
+        itemName: 'walnut',
+        itemStory: 'Oh, look. You found a walnut.'
+    },
+    item2Data: {
+        itemName: 'bagel',
+        itemStory: 'A whole bagel! Lucky crow.'
+    },
+    endpointData: {
+        endpointStory: {
+            unresolved: 'Nope not yet.',
+            resolved: 'You may pass.'
+        }
+    },
+    square1Data: {
+        coords: {
+            x: 0,
+            y: 0
+        },
+        squareDesc: 'You are here. You see things.'
+    },
+    square2Data: {
+        coords: {
+            x: 1,
+            y: 0
+        },
+        squareDesc: 'You are here. You see things.'
+    },
+    level1Data: {
+        levelNum: 1,
+        squares: []
+    },
+    level2Data: {
+        levelNum: 2,
+        squares: []
+    },
+    userData: {
+        name: 'Master Blaster',
+        password: 'bartertown'
+    },
+    hazard1Data: {
+        hazardStory: 'Eep, a fox. You die.'
+    },
+    hazard2Data: {
+        hazardStory: 'Oh no, a cat.'
+    }
+};
