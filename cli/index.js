589dd30db64cf09fa8e34f363a6c3ab059b00c1b
Keli Hansen <keli.p.hansen@gmail.com>
2018-05-07 05:30:08 -0700

    correct url

--- cli/index.js
+++ cli/index.js
@@ -1,7 +1,7 @@
 const request = require('superagent');
 const Game = require('./game');
 const emoji = require('./emoji');
-const server = 'https://better-birds-eye-view.herokuapp.com';
+const server = 'https://best-birds-eye-view.herokuapp.com';
 const colors = require('colors'); // eslint-disable-line
 
 
